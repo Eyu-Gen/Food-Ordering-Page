3ca871554d8ac536fdd329c3f0e8dd6288b52667
Eyu-Gen <dreamersthebig@gmail.com>
2024-07-23 23:16:09 +0545

    Incomplete

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const addtocartBoxes = document.getElementsByClassName("addtocartBox");
 const productQuantities = document.getElementsByClassName("productQuantity");
 const addtocart = document.getElementsByClassName("addtocart");
+const minusBtns = document.getElementsByClassName("minusBtn");
+const productUnits = document.getElementsByClassName("productUnits");
+const itemName = document.getElementsByClassName("itemName");
 const emptyCart = document.getElementById("emptyCart");
 const orderingItems = document.getElementById("orderingItems");
+const itemLists = document.getElementById("itemLists");
 
 let clickedBtn = false;
 for(let i = 0; i < addtocartBoxes.length; i++) {
@@ -15,7 +19,18 @@ for(let i = 0; i < addtocartBoxes.length; i++) {
             addtocart[i].style.display = "none";
             emptyCart.style.display = "none";
             orderingItems.style.display = "block";
+            let error = document.createElement("div");
+            error.innerHTML = `<div></div>`; //Incomplete...
+            itemLists.appendChild(error);
         }
     })
 }
 
+for(let i =0; i < minusBtns.length; i++) {
+    minusBtns[i].addEventListener("click", () => {
+        let unitValue = Number(productUnits[i].value);
+        unitValue++;
+        alert(unitValue);
+        // productUnits[i].innerHTML = unitValue;
+    })
+}
